3b6d6383430fdf6e6dc8c9d3884ccf182174e28f
johanvadenne <johanvadenne@gmail.com>
2023-07-09 21:10:13 +0200

    Update play_v1.3.rb

--- ruby/jeu/course déplcement cube/play_v1.3.rb
+++ ruby/jeu/course déplcement cube/play_v1.3.rb
@@ -10,8 +10,8 @@ class Fenetre < Gosu::Window
         self.update_interval = 1                        # intervalle de mise à jour en milliseconde
         
         # images
-        @img_play = Image.new("image/play.png", Gosu.screen_width / 2, Gosu.screen_height / 2, 0.3, 0.3, 0)    # bouton play
-        @img_souris = Image.new("image/pointer_bleu.png", self.mouse_x / 2, self.mouse_y / 2, 1, 1, 10)        # curseur souris
+        @img_play = Image.new("image/play.png", self, Gosu.screen_width / 2, Gosu.screen_height / 2, 0.3, 0.3, 0, "centre")    # bouton play
+        @img_souris = Image.new("image/pointer_bleu.png", self, self.mouse_x / 2, self.mouse_y / 2, 1, 1, 10, "mouse")        # curseur souris
     end
 
     def update
@@ -19,16 +19,17 @@ class Fenetre < Gosu::Window
         @img_souris.positionX = self.mouse_x    # repositionement de la souris
         @img_souris.positionY = self.mouse_y    # repositionement de la souris
 
-        @img_play.positionX = self.width / 2    # repositionement au centre lors du redimentionement de l'écran
-        @img_play.positionY = self.height / 2   # repositionement au centre lors du redimentionement de l'écran
-        @img_play.scale_agmente(0.005, 0.34) if @img_play.scale < 0.34 && @img_play.survole(mouse_x, mouse_y) # si je survole grossire
+        @img_play.ancrer
+
+        @img_play.scale_agmente(0.005, 0.34) if @img_play.scale < 0.34 && @img_play.survole # si je survole grossire
+        @img_play.scale_egale(0.3) if @img_play.scale != 0.3 && !@img_play.survole # si je survole grossire
 
     end
 
     def draw # dessine
 
-        @img_play.draw
-        @img_souris.draw
+        @img_play.dessiner
+        @img_souris.dessiner
 
     end
 
@@ -39,14 +40,14 @@ class Fenetre < Gosu::Window
     def button_down(id) # récupère le bouton presser
         if id == Gosu::MS_LEFT
             @clique_gauche = true
-            @img_play.presse = true if @img_play.survole(mouse_x, mouse_y)
+            @img_play.presse = true if @img_play.survole
         end
     end
 
     def button_up(id) # récupère le bouton relever
         if id == Gosu::MS_LEFT
             @clique_gauche = false
-            @img_play.clique = true if @img_play.presse == true && @img_play.survole(mouse_x, mouse_y)
+            @img_play.afficher = false if @img_play.presse == true && @img_play.survole
         end
     end
 
@@ -58,68 +59,121 @@ class Fenetre < Gosu::Window
     end
 end
 
-class Image < Gosu::Image
-
-    attr_accessor :positionX, :positionY, :width, :height, :scaleX, :scaleY, :dessiner, :clique, :presse, :opacity
-
-    def initialize(lien, positionX, positionY, scaleX, scaleY, z_index)
-        @image          = Gosu::Image.new(lien)
-        @scaleX         = scaleX
-        @scaleY         = scaleY
-        @width_defaut   = @image.width
-        @height_defaut  = @image.height
-        @width          = @width_defaut * @scaleX
-        @height         = @height_defaut * @scaleY
-        @positionX      = positionX - @width/2
-        @positionY      = positionY - @height/2
-        @z_index        = z_index
-        @dessiner       = true
-        @pressed        = false    
-        @clique         = false
-
+class Image
+    attr_accessor :positionX, :positionY, :width, :height, :scaleX, :scaleY, :z_index, :afficher, :presse
+  
+    def initialize(lien, fenetre, positionX, positionY, scaleX, scaleY, z_index, ancrage)
+      @image = Gosu::Image.new(lien)
+      @image_lien = lien
+      @fenetre = fenetre
+      @scaleX = scaleX
+      @scaleY = scaleY
+      @width_defaut = @image.width
+      @height_defaut = @image.height
+      @width = @width_defaut * scaleX
+      @height = @height_defaut * scaleY
+      @positionX = positionX - @width / 2
+      @positionY = positionY - @height / 2
+      @z_index = z_index
+      @ancrage = ancrage
+      @gauche = @fenetre.width - @positionX
+      @bas = @fenetre.height - @positionY
+      @haut = @positionY - @height/2
+      @afficher = true
+      @presse = false
+  
+    end
+  
+    def dessiner
+        @image.draw(@positionX, @positionY, @z_index, @scaleX, @scaleY) if @afficher
     end
 
-    def draw()
-        @image.draw(@positionX, @positionY, @z_index, @scaleX, @scaleY) if @dessiner
+    def positionX=(valeur)
+        @positionX = valeur - @width/2
     end
 
-    def positionX=(value)
-        @positionX = value - @width/2
+    def positionY=(valeur)
+        @positionY = valeur - @height/2
     end
 
-    def positionY=(value)
-        @positionY = value - @height/2
+    def scaleX=(valeur)
+        modification_scaleX(valeur)
     end
 
-    def scaleX=(value)
-        @scaleX = value
+    def modification_scaleX(valeur)
+        @scaleX = valeur
         ancien_width = @width
         @width = @width_defaut * @scaleX
         @positionX = (@positionX+ancien_width/2) - @width/2
     end
 
-    def scaleY=(value)
-        @scaleY = value
+    def scaleY=(valeur)
+        modification_scaleY(valeur)
+    end
+
+    def modification_scaleY(valeur)
+        @scaleY = valeur
         ancien_height = @height
         @height = @height_defaut * @scaleY
         @positionY = (@positionY+ancien_height/2) - @height/2
     end
-
-    def scale()
-
-        if @scaleX < @scaleY
-            return @scaleX
+  
+    def ancrer
+        nom_ancrage = ["centrer" ,"droite" ,"gauche" ,"haut" ,"haut_gauche" ,"haut_droite" ,"bas_droite" ,"bas_gauche" ,"bas", "mouse"]
+        if @ancrage in nom_ancrage
+            send(@ancrage)
         else
-            return @scaleY
+            puts @ancrage+" n'existe pas!"
         end
-
     end
-
-    def image=(value)
-        @image = Gosu::Image.new(value)
+  
+    def centre
+        @positionX = @fenetre.width / 2 - @width / 2
+        @positionY = @fenetre.height / 2 - @height / 2
     end
-
-    def survole(x, y)
+  
+    def gauche
+        @positionY = @fenetre.height / 2 - @height / 2
+    end
+  
+    def haut_gauche
+        @positionY = @haut
+    end
+  
+    def droite
+        @positionX = (@fenetre.width - @gauche)
+        @positionY = @fenetre.height / 2 - @height / 2
+    end
+  
+    def haut_droite
+        @positionX = (@fenetre.width - @gauche)
+        @positionY = @haut
+    end
+  
+    def haut
+        @positionX = @fenetre.width / 2 - @width / 2
+        @positionY = @haut
+    end
+  
+    def bas
+        @positionX = @fenetre.width / 2 - @width / 2
+        @positionY = (@fenetre.height - @bas) + @height / 2
+    end
+  
+    def bas_droite
+        @positionX = (@fenetre.width - @gauche)
+        @positionY = (@fenetre.height - @bas) + @height / 2
+    end
+  
+    def bas_gauche
+        @positionY = (@fenetre.height - @bas) + @height / 2
+    end
+  
+    def survole
+  
+        x = @fenetre.mouse_x
+        y = @fenetre.mouse_y
+        
         if x > @positionX && x < @positionX + @width && y > @positionY && y < @positionY + @height
             return true
         else
@@ -127,10 +181,20 @@ class Image < Gosu::Image
         end
     end
 
-    def opacity(a)
-        @image.from_blob(width, height, rgba = "\0\0\0\5" * (width * height))
+    def scale
+        return @scaleX
     end
 
+    def scale_agmente(valeur_ajout, valeur_limite)
+        modification_scaleX(@scaleX += valeur_ajout) if @scaleX < valeur_limite
+        modification_scaleY(@scaleY += valeur_ajout) if @scaleY < valeur_limite
+    end
+
+    def scale_egale(valeur)
+        modification_scaleX(@scaleX = valeur)
+        modification_scaleY(@scaleY = valeur)
+    end
+  
 end
 
 class Case_niveau
@@ -142,7 +206,7 @@ class Type_de_jeu
 end
 
 
-type_de_jeu = ["abc","abcABC","abcABC123","abcABC123&%!","personalisé"]
+type_de_jeu = ["lettre aléatoire","phrase aléatoire"]
 
 fenetre_game = Fenetre.new()
 fenetre_game.show
