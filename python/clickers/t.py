ef83abe1b63e03a3495c206eb1ea7b12f0e0c5d3
johanvadenne <johanvadenne@gmail.com>
2023-09-08 21:52:17 +0200

    Update t.py

--- python/clickers/t.py
+++ python/clickers/t.py
@@ -11,16 +11,5 @@ def pressLeft():
     mouse.release(Button.left)
 
 
-for i in range(100):
-    time.sleep(1.5)
-    mouse.position = (934, 502)
-    pressRight()
-    time.sleep(0.2)
-    mouse.position = (998, 784)
-    pressLeft()
-    time.sleep(0.5)
-    mouse.position = (511, 484)
-    pressLeft()
-    time.sleep(0.5)
-    mouse.position = (1821, 504)
-    pressLeft()
+time.sleep(2)
+print(mouse.position)
